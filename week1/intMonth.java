c3b95606927a7c002fe904c42c653ed4d835d13d
unknown <laiquan163@gmail.com>
2023-10-12 16:38:27 +0700

    message

--- week1/intMonth.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class intMonth {
-	public static void main(String[] args) {
-		// Tạo ra đối tượng sc thuộc lớp Scanner để nhập dữ liệu
-		Scanner sc = new Scanner(System.in);
-		String name = sc.next();
-		System.out.println("Hello " + name);
-	}
-}
