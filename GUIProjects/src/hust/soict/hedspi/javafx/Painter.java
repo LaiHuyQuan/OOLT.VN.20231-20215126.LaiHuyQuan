8180449e92d6fe83325b6a901f28694e82fe6f73
unknown <laiquan163@gmail.com>
2023-12-14 17:05:48 +0700

    Painter updated

---
+++ GUIProjects/src/hust/soict/hedspi/javafx/Painter.java
@@ -0,0 +1,26 @@
+package hust.soict.hedspi.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Painter extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/hust/soict/hedspi/javafx.Painter.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Painter");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
